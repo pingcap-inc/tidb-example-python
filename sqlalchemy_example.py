c41b59002d09444464242d57803869d83d702dc8
Icemap <cheese.javaee@gmail.com>
2022-11-17 02:13:15 +0800

    [Update] add SQLAlchemy example

---
+++ sqlalchemy_example.py
@@ -0,0 +1,129 @@
+# Copyright 2022 PingCAP, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import uuid
+
+from sqlalchemy import create_engine, String, Column, Integer, select, func
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+engine = create_engine('mysql://root:@127.0.0.1:4000/test', echo=True)
+Base = declarative_base()
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+
+
+class Player(Base):
+    __tablename__ = "player"
+
+    id = Column(String(36), primary_key=True)
+    coins = Column(Integer)
+    goods = Column(Integer)
+
+    def __repr__(self):
+        return f'Player(id={self.id!r}, coins={self.coins!r}, goods={self.goods!r})'
+
+
+def random_player(amount: int):
+    players = []
+    for _ in range(amount):
+        players.append(Player(id=uuid.uuid4(), coins=10000, goods=10000))
+
+    return players
+
+
+def simple_example():
+    with Session() as session:
+        # create a player, who has a coin and a goods.
+        session.add(Player(id="test", coins=1, goods=1))
+
+        # get this player, and print it.
+        get_test_stmt = select(Player).where(Player.id == "test")
+        for player in session.scalars(get_test_stmt):
+            print(player)
+
+        # create players with bulk inserts.
+        # insert 1919 players totally, with 114 players per batch.
+        # all players have random uuid
+        player_list = random_player(1919)
+        for idx in range(0, len(player_list), 114):
+            session.bulk_save_objects(player_list[idx:idx + 114])
+
+        # print the number of players
+        count = session.query(func.count(Player.id)).scalar()
+        print(f'number of players: {count}')
+
+        # print 3 players.
+        three_players = session.query(Player).limit(3).all()
+        for player in three_players:
+            print(player)
+
+        session.commit()
+
+
+def trade_check(session, sell_id: str, buy_id: str, amount: int, price: int):
+    # sell player goods check
+    sell_player = session.query(Player.goods).filter(Player.id == sell_id).with_for_update().one()
+    if sell_player.goods < amount:
+        print(f'sell player {sell_id} goods not enough')
+        return False
+
+    # buy player coins check
+    buy_player = session.query(Player.coins).filter(Player.id == buy_id).with_for_update().one()
+    if buy_player.coins < price:
+        print(f'buy player {buy_id} coins not enough')
+        return False
+
+
+def trade(sell_id: str, buy_id: str, amount: int, price: int):
+    with Session() as session:
+        if trade_check(session, sell_id, buy_id, amount, price) is False:
+            return
+
+        # deduct the goods of seller, and raise his/her the coins
+        session.query(Player).filter(Player.id == sell_id). \
+            update({'goods': Player.goods - amount, 'coins': Player.coins + price})
+        # deduct the coins of buyer, and raise his/her the goods
+        session.query(Player).filter(Player.id == buy_id). \
+            update({'goods': Player.goods + amount, 'coins': Player.coins - price})
+
+        session.commit()
+        print("trade success")
+
+
+def trade_example():
+    with Session() as session:
+        # create two players
+        # player 1: id is "1", has only 100 coins.
+        # player 2: id is "2", has 114514 coins, and 20 goods.
+        session.add(Player(id="1", coins=100, goods=0))
+        session.add(Player(id="2", coins=114514, goods=20))
+        session.commit()
+
+    # player 1 wants to buy 10 goods from player 2.
+    # it will cost 500 coins, but player 1 cannot afford it.
+    # so this trade will fail, and nobody will lose their coins or goods
+    trade(sell_id="2", buy_id="1", amount=10, price=500)
+
+    # then player 1 has to reduce the incoming quantity to 2.
+    # this trade will successful
+    trade(sell_id="2", buy_id="1", amount=2, price=100)
+
+    with Session() as session:
+        traders = session.query(Player).filter(Player.id.in_(("1", "2"))).all()
+        for player in traders:
+            print(player)
+        session.commit()
+
+simple_example()
+trade_example()
