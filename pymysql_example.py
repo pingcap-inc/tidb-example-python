45016d30d215b0289cf2909667c47f613246a793
Icemap <cheese.javaee@gmail.com>
2022-11-17 15:33:06 +0800

    [Update] add pymysql example

---
+++ pymysql_example.py
@@ -0,0 +1,157 @@
+# Copyright 2022 PingCAP, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import uuid
+
+import pymysql.cursors
+from pymysql import Connection
+
+
+def get_connection(autocommit: bool = False) -> Connection:
+    return pymysql.connect(host='127.0.0.1',
+                           port=4000,
+                           user='root',
+                           password='',
+                           database='test',
+                           cursorclass=pymysql.cursors.DictCursor,
+                           autocommit=autocommit)
+
+
+def create_player(cursor, player: tuple):
+    cursor.execute("INSERT INTO player (id, coins, goods) VALUES (%s, %s, %s)", player)
+
+
+def get_player(cursor, player_id: str):
+    cursor.execute("SELECT id, coins, goods FROM player WHERE id = %s", (player_id,))
+    return cursor.fetchone()
+
+
+def get_players_with_limit(cursor, limit: int):
+    cursor.execute("SELECT id, coins, goods FROM player LIMIT %s", (limit,))
+    return cursor.fetchall()
+
+
+def random_player(amount: int):
+    players = []
+    for _ in range(amount):
+        players.append((uuid.uuid4(), 10000, 10000))
+
+    return players
+
+
+def bulk_create_player(cursor, players: list):
+    cursor.executemany("INSERT INTO player (id, coins, goods) VALUES (%s, %s, %s)", players)
+
+
+def get_count(cursor):
+    cursor.execute("SELECT count(*) as count FROM player")
+    return cursor.fetchone()['count']
+
+
+def trade_check(cursor, sell_id: str, buy_id: str, amount: int, price: int):
+    get_player_with_lock_sql = "SELECT coins, goods FROM player WHERE id = %s FOR UPDATE"
+
+    # sell player goods check
+    cursor.execute(get_player_with_lock_sql, (sell_id,))
+    seller = cursor.fetchone()
+    if seller['goods'] < amount:
+        print(f'sell player {sell_id} goods not enough')
+        return False
+
+    # buy player coins check
+    cursor.execute(get_player_with_lock_sql, (buy_id,))
+    buyer = cursor.fetchone()
+    if buyer['coins'] < price:
+        print(f'buy player {buy_id} coins not enough')
+        return False
+
+
+def trade_update(cursor, sell_id: str, buy_id: str, amount: int, price: int):
+    update_player_sql = "UPDATE player set goods = goods + %s, coins = coins + %s WHERE id = %s"
+
+    # deduct the goods of seller, and raise his/her the coins
+    cursor.execute(update_player_sql, (-amount, price, sell_id))
+    # deduct the coins of buyer, and raise his/her the goods
+    cursor.execute(update_player_sql, (amount, -price, buy_id))
+
+
+def trade(connection, sell_id: str, buy_id: str, amount: int, price: int):
+    with connection.cursor() as cursor:
+        if trade_check(cursor, sell_id, buy_id, amount, price) is False:
+            connection.rollback()
+            return
+
+        try:
+            trade_update(cursor, sell_id, buy_id, amount, price)
+        except Exception as err:
+            connection.rollback()
+            print(f'something went wrong: {err}')
+        else:
+            connection.commit()
+            print("trade success")
+
+
+def simple_example():
+    with get_connection(autocommit=True) as connection:
+        with connection.cursor() as cur:
+            # create a player, who has a coin and a goods.
+            create_player(cur, ("test", 1, 1))
+
+            # get this player, and print it.
+            test_player = get_player(cur, "test")
+            print(test_player)
+
+            # create players with bulk inserts.
+            # insert 1919 players totally, with 114 players per batch.
+            # all players have random uuid
+            player_list = random_player(1919)
+            for idx in range(0, len(player_list), 114):
+                bulk_create_player(cur, player_list[idx:idx + 114])
+
+            # print the number of players
+            count = get_count(cur)
+            print(f'number of players: {count}')
+
+            # print 3 players.
+            three_players = get_players_with_limit(cur, 3)
+            for player in three_players:
+                print(player)
+
+
+def trade_example():
+    with get_connection(autocommit=False) as connection:
+        with connection.cursor() as cur:
+            # create two players
+            # player 1: id is "1", has only 100 coins.
+            # player 2: id is "2", has 114514 coins, and 20 goods.
+            create_player(cur, ("1", 100, 0))
+            create_player(cur, ("2", 114514, 20))
+            connection.commit()
+
+        # player 1 wants to buy 10 goods from player 2.
+        # it will cost 500 coins, but player 1 cannot afford it.
+        # so this trade will fail, and nobody will lose their coins or goods
+        trade(connection, sell_id="2", buy_id="1", amount=10, price=500)
+
+        # then player 1 has to reduce the incoming quantity to 2.
+        # this trade will successful
+        trade(connection, sell_id="2", buy_id="1", amount=2, price=100)
+
+        # let's take a look for player 1 and player 2 currently
+        with connection.cursor() as cur:
+            print(get_player(cur, "1"))
+            print(get_player(cur, "2"))
+
+
+simple_example()
+trade_example()
